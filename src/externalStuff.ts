e0ff2f5be11cbaadd1f2c9300217116a1919317b
ajhalim <thenubbum@gmail.com>
2023-12-14 00:19:35 -0800

    Revert "basic really bad translation"
    This reverts commit 94641e98042bc7e8923d5cbc514ff9b486791b4d.

--- src/externalStuff.ts
+++ src/externalStuff.ts
@@ -7,29 +7,28 @@ import {numTiles, cellData} from './main.ts'
     playerPos: [];
 }  */
 
-export function reader(file:string){
+export function reader(){
     //qlet thing 
     //console.log(data.Scenario.winCondition);
-    const obj = JSON.parse(file);
-    /* let currentSituation = {
-        winCond: file.Scenario.winCondition,
-        playerPos: file.Scenario.playerPos,
+    let currentSituation = {
+        winCond: data.Scenario.winCondition,
+        playerPos: data.Scenario.playerPos,
 
-        event1Name: file.Scenario.event1.name,
-        event1Start: file.Scenario.event1.when,
-        event1Water: file.Scenario.event1.waterLevel,
-        event1Sun: file.Scenario.event1.sunLevel,
+        event1Name: data.Scenario.event1.name,
+        event1Start: data.Scenario.event1.when,
+        event1Water: data.Scenario.event1.waterLevel,
+        event1Sun: data.Scenario.event1.sunLevel,
 
-        event2Name: file.Scenario.event2.name,
-        event2Start: file.Scenario.event2.when,
-        event2Water: file.Scenario.event2.waterLevel,
-        event2Sun: file.Scenario.event2.sunLevel,
+        event2Name: data.Scenario.event2.name,
+        event2Start: data.Scenario.event2.when,
+        event2Water: data.Scenario.event2.waterLevel,
+        event2Sun: data.Scenario.event2.sunLevel,
 
-        event3Name: file.Scenario.event3.name,
-        event3Start: file.Scenario.event3.when,
-        event3Water: file.Scenario.event3.waterLevel,
-        event3Sun: file.Scenario.event3.sunLevel,
-    } */
+        event3Name: data.Scenario.event3.name,
+        event3Start: data.Scenario.event3.when,
+        event3Water: data.Scenario.event3.waterLevel,
+        event3Sun: data.Scenario.event3.sunLevel,
+    }
     //currentSituation = 1;
     //currentSituation.winCond = 10;
     //currentSituation.playerPos = data.Scenario.playerPos;
